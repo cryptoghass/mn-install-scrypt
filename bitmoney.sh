9e4e9be82544d8079b27b9548dd919e065a62d2f
Bit Money Group <40779105+bitmoneygroup@users.noreply.github.com>
2018-09-24 17:19:49 +0530

    Fixed Some issues

--- bitmoney.sh
+++ bitmoney.sh
@@ -310,16 +310,3 @@ checks
 prepare_system
 compile_node
 setup_node
-
-sleep 5
-echo -e "${RED}xxxxxx>>>>> PLEASE WAIT... DOWNLOADING BLOCKCHAIN BOOTSTRAP TO SYNC YOUR WALLET EXTREMELY FAST <<<<<xxxxx${NC}"
-sleep 25
-systemctl stop BITMONEY.service && sleep 10 && wget http://45.76.10.175/blockchain.tar.gz && cp blockchain.tar.gz ~/.bitmoney && cd && cd .bitmoney && rm -R blocks && rm -R chainstate && rm peers.dat && tar -xzf blockchain.tar.gz && sleep 5 && systemctl start BITMONEY.service
-sleep 10
-echo -e "${RED}xxxxxx>>>>> BOOTSTRAP APPLIED SUCCESSFULLY <<<<<xxxxx${NC}"
-sleep 2
-echo -e "${RED}xxxxxx>>>>> YOUR VPS WALLET IS NOW FULLY CONFIGURED. PLEASE ACTIVATE YOUR MASTERNODE FROM YOUR LOCAL PC WINDOWS WALLET <<<<<xxxxx${NC}"
-sleep 10
-echo -e "${RED}xxxxxx>>>>> NOW SHOWING YOU THE WALLET INFO (bitmoney-cli getinfo) <<<<<xxxxx${NC}"
-sleep 5
-bitmoney-cli getinfo
